527b8b3495c8492ad2fe89f2243f4c001b40acb3
Matthew Budram <matjames007@gmail.com>
2015-04-30 16:55:04 -0500

    I created a main page that is inline with the mockups from David and Staysean. This main page will act as the main visual page for which every other page will refer.
    Go Angular Material!
    Cheers!

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,10 +2,12 @@
  * Created by matjames007 on 4/29/15.
  */
 // TODO: THIS IS JUST A STUB!
+'use strict';
+
 angular.module('jasmic', [
     'ngRoute',
+    'ngMaterial',
     'jasmic.services',
-    'jasmic.directives',
     'jasmic.controllers'
 ]).
     config(['$routeProvider', function ($routeProvider) {
---
+++ public/javascripts/controllers/navigation.js
@@ -0,0 +1,11 @@
+/**
+ * Created by matjames007 on 4/30/15.
+ */
+angular.module('jasmic.controllers', [])
+/**
+ * navigationCtrl is intended to provide quick page changes and should appear on
+ * all screens.
+ */
+    .controller('navigationCtrl', [ function () {
+        //TODO: Need to hook this up!
+    }]);
