cc42e04272e1fe3569a5b82420f73baf540730b5
Matthew Budram <matjames007@gmail.com>
2015-05-07 14:57:23 -0500

    Ok, we reached a milestone with farmer information popping up on click!
    I used a partials template to load a small farmer profile when a table
    item has been clicked.  This can be reused on other screens.
    
    Cheers!

--- public/javascripts/controllers/search-ctrl.js
+++ public/javascripts/controllers/search-ctrl.js
@@ -7,9 +7,15 @@ angular.module('jasmic.controllers')
         function ($scope, $location, $routeParams, SearchAllFactory) {
             $scope.results = SearchAllFactory.query($routeParams);
             $scope.terms = $routeParams.searchTerms;
+            $scope.selected = false;
 
             $scope.searchAll = function() {
                 $location.url("/search?searchTerms=" + $scope.search);
-            }
+            };
+
+            $scope.selectedElement = function(farmer) {
+                $scope.selectedFarmer = farmer;
+                $scope.selected = true;
+            };
         }
     ]);
