973f8aa4f5b9542b3cd5231adebc479daffcdb81
Matthew Budram <matjames007@gmail.com>
2015-05-08 14:39:45 -0500

    Just enabled the small farmer-view window for the farmer listing page.

--- public/javascripts/controllers/farmer-ctrl.js
+++ public/javascripts/controllers/farmer-ctrl.js
@@ -6,5 +6,11 @@ angular.module('jasmic.controllers')
     .controller('FarmerListingCtrl', ['$scope', '$routeParams', 'FarmersFactory',
         function ($scope, $routeParams, FarmersFactory) {
             $scope.farmers = FarmersFactory.query($routeParams);
+            $scope.selected = false;
+
+            $scope.selectedElement = function(farmer) {
+                $scope.selectedFarmer = farmer;
+                $scope.selected = true;
+            };
         }
     ]);
