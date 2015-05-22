cbf7a7acfe54f9c0a6e3cbe4b5dadab14a091c34
Matthew Budram <matjames007@gmail.com>
2015-05-22 09:35:50 -0500

    Adding the floating card functionality to the active demand page of the application.
    Matthew Budram

--- public/javascripts/controllers/demand-ctrl.js
+++ public/javascripts/controllers/demand-ctrl.js
@@ -4,12 +4,27 @@
 
 angular.module('jasmic.controllers')
     .controller('DemandListingCtrl', ['$scope','$location','$routeParams', 'CurrentDemandsFactory',
-        function ($scope, $location, $routeParams, CurrentDemandsFactory) {
+        'DemandMatchFactory',
+        function ($scope, $location, $routeParams, CurrentDemandsFactory, DemandMatchFactory) {
             CurrentDemandsFactory.query({}, function(demands) {
                 $scope.demands = demands;
             },
             function(error) {
                 $scope.demands = [];
             });
+
+            $scope.selectedItem = function(demand) {
+                $scope.selectedDemand = demand;
+                $scope.itemSelected = true;
+                lookupDemandMatches();
+            };
+
+            $scope.itemSelected = false;
+
+            lookupDemandMatches = function() {
+                DemandMatchFactory.query({id: $scope.selectedDemand._id}, function(list) {
+                    $scope.m_commodities = list;
+                })
+            }
         }
     ]);
