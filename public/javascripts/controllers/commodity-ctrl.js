2f82267182b94596390a5c1d76f7c71e96131dc2
Matthew Budram <matjames007@gmail.com>
2015-05-22 18:11:41 -0500

    I am adding documentation to the commodity-ctrl.

--- public/javascripts/controllers/commodity-ctrl.js
+++ public/javascripts/controllers/commodity-ctrl.js
@@ -6,6 +6,9 @@ angular.module('jasmic.controllers')
     .controller('CommodityListingCtrl', ['$scope','$location','$routeParams', 'CurrentCommoditiesFactory',
         'CommodityMatchFactory',
         function ($scope, $location, $routeParams, CurrentCommoditiesFactory, CommodityMatchFactory) {
+            /**
+             * This queries the database for the current active commodities
+             */
             CurrentCommoditiesFactory.query({}, function(commodities) {
                 $scope.commodities = commodities;
             },
@@ -13,12 +16,21 @@ angular.module('jasmic.controllers')
                 $scope.commodities = [];
             });
 
+            /**
+             * trigger a click action to retrieve the item from the table
+             * for the lookup.
+             * @param commodity
+             */
             $scope.selectedItem = function(commodity) {
                 $scope.selectedCommodity = commodity;
                 $scope.itemSelected = true;
                 lookupCommodityMatches();
             };
 
+            /**
+             * The front end uses this to adjust the the screen to accomodate the floating card
+             * @type {boolean}
+             */
             $scope.itemSelected = false;
 
             lookupCommodityMatches = function() {
