73554afad3bef967abee676993d9bdede6c0e936
Matthew Budram <matjames007@gmail.com>
2015-07-02 22:33:44 -0500

    Revert "I have finally gotten the implementation to look close to what the design that David has done for the dashboard. Currently only the open demands is hooked up."
    This reverts commit 12653ffa809d03b5cf069b6eebc8529ab81ea03d.

--- public/javascripts/controllers/dashboard-ctrl.js
+++ public/javascripts/controllers/dashboard-ctrl.js
@@ -5,34 +5,11 @@
 angular.module('jasmic.controllers')
     .controller('DashboardCtrl', ['$scope','$location','$routeParams', 'CurrentDemandsFactory',
         function ($scope, $location, $routeParams, CurrentDemandsFactory) {
-            CurrentDemandsFactory.query( function(demands) {
+            CurrentDemandsFactory.query({amount: 5}, function(demands) {
                     $scope.demands = demands;
                 },
                 function(error) {
                     $scope.demands = [];
                 });
-
-            /**
-             * States of the drop down - false = closed
-             * @type {{demand: boolean, calls: boolean, transactions: boolean}}
-             */
-            $scope.states = {
-                demand: false,
-                calls: false,
-                transactions: false
-            };
-
-            /**
-             * sets the states of the drop down menus
-             * @param item
-             */
-            $scope.setSelected = function(item) {
-                var temp = $scope.states[item];
-                for(i in $scope.states) {
-                    $scope.states[i] = false;
-                }
-                $scope.states[item] = temp;
-                $scope.states[item] = !$scope.states[item];
-            }
         }
     ]);
