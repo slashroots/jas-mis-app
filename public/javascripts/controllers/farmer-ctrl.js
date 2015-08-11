c4348c52864f0e4dca5464d444d383136a2e51cd
Nick Williams <nick22891@yahoo.com>
2015-08-11 11:19:51 -0400

    This fixes issue # 88.

--- public/javascripts/controllers/farmer-ctrl.js
+++ public/javascripts/controllers/farmer-ctrl.js
@@ -90,6 +90,14 @@ angular.module('jasmic.controllers')
                 }
             };
 
+            $scope.isActive = function(exp_date) {
+                if(new Date(exp_date) > Date.now()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            };
+
             /**
              * Attempts to add new Farm to the farmer object.  Assumes the
              * server will take care of the address creation.
