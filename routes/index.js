b98975db7558c6bcf2edef22b0dc5116b25f0194
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2015-08-17 11:11:20 -0500

    changing logic for user creation

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var moment = require('moment');
 var router = express.Router();
-var NewUser = require('../models/db').NewUser;
+var NewUser = require('../models/db').NewUser,
+    User = require('../models/db').User;
 
 
 /* GET home page. */
@@ -34,10 +35,42 @@ router.get('/user/:id/new', function(req, res){
     if(err || !user){
       res.render('user', {found: false, title: 'New User'});
     }else{
-      res.render('user', {found: true, title: 'New User'});
+      res.render('user', {found: true, title: 'New User', id: req.params.id});
     }
   });
 });
 
+/**
+ * Creates a new user once the new user has successfully
+ * updated default password.
+ * TODO - Get original password from record and compare to
+ * new password.
+ */
+router.post('/user/:id/confirm', function(req, res){
+  NewUser.findById(req.params.id, function(err, new_user){
+    if(err || !new_user){
+      res.redirect('/user/'+req.params.id+'/new', {found: false});
+    }else{
+      var user = new User(new_user);
+      user.us_password = req.body.password;
+      user.us_verified = "Verified";
+      user.save(function(err){
+        if(err){
+          res.redirect('/user/'+req.params.id+'/new', {found: false});
+        }else{
+          res.redirect('/login');
+        }
+      });
+    }
+  });
+  //NewUser.findById(req.params.id, {$set: {us_status: "Verified", us_password: req.body.password}}, function(err, result){
+  //  if(err || !result){
+  //    res.redirect('/user/'+req.params.id+'/new', {found: false});
+  //  }else{
+  //    new User()
+  //    res.redirect('/login');
+  //  }
+  //});
+});
 
 module.exports = router;
